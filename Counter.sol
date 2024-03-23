a87de80ef1905bde63231f2180e25dd167dfe94b
andrewkhachaturyan <162831036+andrewkhachaturyan@users.noreply.github.com>
2024-03-23 12:08:22 +0400

    Create Counter.sol

---
+++ Counter.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.17;
+
+// SPDX-License-Identifier: MIT
+
+contract Counter {
+  string public name = "Counter";
+  string public symbol = "CER";
+  uint8 public decimals = 18;
+  uint256 public totalSupply = 10000000000;
